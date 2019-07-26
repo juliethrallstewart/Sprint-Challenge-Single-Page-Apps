77906c5ebb4163fad9a4be9c59d2699a9bbbc6b4
Julie Thrall <juliethrall10@gmail.com>
2019-07-26 10:37:20 -0700

    nav completed

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import TabNav from './components/TabNav.js';
 import Header from './components/Header.js';
 import AppRouter from './components/AppRouter.js';
 import CharacterList from './components/CharacterList';
+import { Route, Switch } from 'react-router-dom';
+
 
 export default function App() {
   return <main>
     <Header />
     <TabNav />
-    <CharacterList />
     <AppRouter />
   </main>
 }
